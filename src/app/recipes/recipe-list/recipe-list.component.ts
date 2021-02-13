851ed75f652b57bea6e31efd7397417b2d8a7293
turbo6412 <anthonyvuong@csu.fullerton.edu>
2021-02-12 20:08:54 -0800

    created a recipe class object to be instantiated in other files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+  'https://chowhound3.cbsistatic.com/recipe_photos/29655_paella_mixta.jpg'),
+  new Recipe('A Second Test Recipe', 'This is another test',
+  'https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg')
+
+];
+
 
   constructor() { }
 
