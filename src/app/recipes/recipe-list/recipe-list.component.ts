b2e900b4c6e0eef858755cec024249d27610cff0
turbo6412 <anthonyvuong@csu.fullerton.edu>
2021-06-08 22:01:55 -0700

    created a chain of even emitters to control what recipe detail component displays based on the recipe selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,21 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test',
   'https://chowhound3.cbsistatic.com/recipe_photos/29655_paella_mixta.jpg'),
   new Recipe('A Second Test Recipe', 'This is another test',
-  'https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg'),
-  new Recipe('A Third Test Recipe', 'This is a bad test',
-  'https://orientalsouls.com/wp-content/uploads/2018/08/KRevoSushi01.jpg')
-
-
+  'https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg')
 ];
-
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
